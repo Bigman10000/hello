a12239d57c279487d892a3e2deffaa37bb27aa6d
Bigman10000 <youremail@huskers.unl.edu>
2024-08-29 12:56:00 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ian Gouldie
+ *         igouldie2@huskers.unl.edu
+ * Date: 08/29/2024
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ian Gouldie\n");
+
+  return 0;
+}
